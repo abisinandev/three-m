a44666e8af06d338cc68661adaac51811e9a1a97
Abi <183334201+abisinandev@users.noreply.github.com>
2026-04-13 12:51:56 +0530

    feat(backend): subscription routes changed to constant

--- server/src/presentation/http/routes/admin/admin-subscription-management.routes.ts
+++ server/src/presentation/http/routes/admin/admin-subscription-management.routes.ts
@@ -3,16 +3,18 @@ import { container } from '@infrastructure/inversify_di/container';
 import { SUBSCRIPTION_TYPES } from '@infrastructure/inversify_di/features/subscription/subscription.types';
 import { validateDTO } from '@presentation/express/middlewares/validation-dto.middlewares';
 import { AdminSubscriptionManagementController } from '@presentation/http/controllers/admin/admin-subscription-management.controller';
+import { AdminSubscriptionRoutes } from '@shared/routes/admin.routes';
 import { Router } from 'express';
+
 const router = Router();
 
 const controller = container.get<AdminSubscriptionManagementController>(
     SUBSCRIPTION_TYPES.AdminSubscriptionManagementController
-)
+);
 
-router.get("/", controller.getPlans.bind(controller));
-router.get("/stats", controller.getStats.bind(controller));
-router.get("/all-subscriptions", controller.getSubscriptions.bind(controller));
-router.patch("/:code", validateDTO(UpdatePlanDTO), controller.updatePlan.bind(controller));
+router.get(AdminSubscriptionRoutes.FETCH_PLANS, controller.getPlans.bind(controller));
+router.get(AdminSubscriptionRoutes.FETCH_STATS, controller.getStats.bind(controller));
+router.get(AdminSubscriptionRoutes.FETCH_SUBSCRIPTIONS, controller.getSubscriptions.bind(controller));
+router.patch(AdminSubscriptionRoutes.UPDATE_PLAN, validateDTO(UpdatePlanDTO), controller.updatePlan.bind(controller));
 
 export default router;
