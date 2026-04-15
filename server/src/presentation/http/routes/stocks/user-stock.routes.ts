3e52f3f7a60d0f6ea0da1b603e33d2c8f52246fa
Abi <183334201+abisinandev@users.noreply.github.com>
2026-04-15 19:31:26 +0530

    feat(backend): constant route setup

--- server/src/presentation/http/routes/stocks/user-stock.routes.ts
+++ server/src/presentation/http/routes/stocks/user-stock.routes.ts
@@ -1,13 +1,14 @@
 import { container } from "@infrastructure/inversify_di/container";
 import { STOCK_TYPES } from "@infrastructure/inversify_di/features/stock/stock.types";
 import { UserStocksController } from "@presentation/http/controllers/stocks/user-stocks.controller";
+import { StockTradingRoutes } from "@shared/routes/stock-trading.routes";
 import { Router } from "express";
 
 const router = Router();
 const userStocksController = container.get<UserStocksController>(STOCK_TYPES.UserStocksController);
 
-router.get('/', userStocksController.getStocks.bind(userStocksController));
-router.get('/:symbol', userStocksController.getStockDetails.bind(userStocksController));
-router.get('/:symbol/candles', userStocksController.getStockCandles.bind(userStocksController));
+router.get(StockTradingRoutes.DEFAUTL, userStocksController.getStocks.bind(userStocksController));
+router.get(StockTradingRoutes.GET_STOCKS, userStocksController.getStockDetails.bind(userStocksController));
+router.get(StockTradingRoutes.GET_CANDLES, userStocksController.getStockCandles.bind(userStocksController));
 
 export default router;
