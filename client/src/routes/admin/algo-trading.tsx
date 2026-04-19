ffbb90afcd1be9c982e42fe71cc51337f27203b0
Abi <183334201+abisinandev@users.noreply.github.com>
2026-04-19 12:30:36 +0530

    fix(frontend): algo-trading conflict resolved

--- client/src/routes/admin/algo-trading.tsx
+++ client/src/routes/admin/algo-trading.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import AlgoTradingAdminPage from '@/modules/admin/page/AlgoTradingAdminPage';
-=======
 import AlgoTradingPage from '@/modules/admin/page/AlgoTradingPage';
->>>>>>> 8d49cdd4b26558c6250c039ede81287f10c72c34
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/admin/algo-trading')({
