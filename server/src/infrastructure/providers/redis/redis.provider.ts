9924323ed9d662899069c01d115e88ad3cc174f6
Abi <183334201+abisinandev@users.noreply.github.com>
2026-05-17 20:29:22 +0530

    fix: docker redis configs issue solved

--- server/src/infrastructure/providers/redis/redis.provider.ts
+++ server/src/infrastructure/providers/redis/redis.provider.ts
@@ -3,11 +3,8 @@ import Redis from "ioredis";
 import { logger } from "../logger/pino.logger";
 
 
-export const redisClient = new Redis({
-    host: "localhost",
-    port: Number(env.REDIS_PORT),
-    db: 0,
-});
+export const redisClient = new Redis(env.REDIS_URL);
 
 redisClient.on("connect", () => logger.info("Redis connected"));
 redisClient.on("error", (err) => logger.error(`Redis error: ${err}`));
+
